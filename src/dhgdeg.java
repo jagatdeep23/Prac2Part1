d3a33b5eccaefc773aa57d9496e9354eda889a62
jc277043 <jagatdeepchohan@gmail.com>
2015-03-09 15:53:54 +1000

    chakde fatte

---
+++ src/dhgdeg.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jc277043 on 9/03/15.
+ */
+public class dhgdeg {
+    public static void main(String[] args) {
+        String text = "Thisisasimpletest\nOneTwoThree\nHelloworld!";
+        for (int i = 0; i < text.length(); ++i) {
+            System.out.print(text.charAt(i));
+        }
+        System.out.println();
+    }
+}
